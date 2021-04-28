1855dc28c74a1f82d2e3d374e24a24b3c04aad7a
JPB <jbanas9124@gmail.com>
2021-04-28 09:40:16 -0500

    employee.js, role.js, department.js all connect to corresponding tables

--- routes/apiRoutes/server.js
+++ routes/apiRoutes/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 router.use(require('./department'));
+router.use(require('./role'));
+router.use(require('./employee'));
 
 
 module.exports = router;
