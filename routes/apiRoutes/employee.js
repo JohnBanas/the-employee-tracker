1d437ae5c04ef8c91885a004ff61949a28c22c84
JPB <jbanas9124@gmail.com>
2021-04-28 15:18:53 -0500

    created api for updating employee's role

--- routes/apiRoutes/employee.js
+++ routes/apiRoutes/employee.js
@@ -35,6 +35,66 @@ router.get(`/employee/:id`, (req, res) => {
   });
 });
 
+//add employee to database
+router.post('/employee', ({ body }, res) => {
+  const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id)
+  VALUES (?,?,?,?)`;
+  const params = [body.first_name, body.last_name, body.role_id, body.manager_id];
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'Successfully added candidate!',
+      data: body
+    });
+  });
+});
+
+//delete employee
+router.delete('/employee/:id', (req, res) => {
+  const sql = `DELETE FROM employee WHERE id = ?`
+  const params = [req.params.id];
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'Sorry, this candidate was not found.'
+      });
+    } else {
+      res.json({
+        message: 'Successfully deleted.',
+        changes: result.affectedRows,
+        id: req.params.id
+      });
+    }
+  });
+});
+
+//update employees role
+router.put(`/employee/:id`, ({body}, res) => {
+  const sql = `UPDATE employee
+  SET first_name = ?,
+  last_name =?,
+  role_id = ?,
+  manager_id =?
+  WHERE id = ?`;
+  const params = [body.first_name, body.last_name, body.role_id, body.manager_id, body.id];
+  db.query(sql, params, (err, results, fields) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json(
+      {
+        message: 'Success!',
+        data: results
+      })
+  });
+});
+
 
 
 
