2c0fbbd075c361793238ff389d16c972e98a7aa5
JPB <jbanas9124@gmail.com>
2021-04-27 23:42:13 -0500

    initial file setup and sql setup

---
+++ db/seeds.sql
@@ -0,0 +1,32 @@
+INSERT INTO department (name)
+VALUES
+('sales'),
+('engineering'),
+('financial'),
+('human resources');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+('Sales Team Member', 4500000, 1),
+('Sales Team Lead', 6500000, 1),
+('Software Engineer', 7500000, 2),
+('Senior Software Engineer', 9500000, 2),
+('Accountant', 5500000, 3),
+('Account Manager', 8500000, 3),
+('Human Resources', 6500000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+('John', 'Abasam', 2, NULL),
+('Jane', 'Abasam', 1, 1),
+('Joe', 'Actual', 3, 6),
+('Jackie', 'Awesome', 3, 6),
+('Jennifer', 'Avidavid', 3, 6),
+('Joseph', 'Allstate', 4, NULL),
+('Josh', 'Acton', 5, 12),
+('Jonah', 'Afernabish', 5, 12),
+('Jessica', 'Astel', 5, 12),
+('Jasper', 'Angelinco', 3, 6),
+('Jim', 'Alinter', 5, 12),
+('Jacob', 'Asmat', 6, NULL),
+('Jolene', 'Avistor', 7, NULL);
