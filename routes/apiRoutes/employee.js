166d37dbf11473281bf5b5cc7fd366abc7e1d8ca
JPB <jbanas9124@gmail.com>
2021-04-29 01:40:42 -0500

    created api routes for several functions and variations of employee selection

--- routes/apiRoutes/employee.js
+++ routes/apiRoutes/employee.js
@@ -18,6 +18,99 @@ router.get('/employee', (req, res) => {
   });
 });
 
+//get employees by manager
+router.get('/employee/manager/:id', (req, res) => {
+  let params = [req.params.id];
+  const sql = `SELECT * FROM employee WHERE manager_id = ?`;
+  db.query(sql, params, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json(
+      {
+        message: 'Success!',
+        data: rows
+      })
+  });
+});
+
+//get employees by department
+router.get('/employee/department/:id', (req, res) => {
+  let params = req.params.id;
+  let sql = `SELECT * FROM department WHERE id = ?`;
+  db.query(sql, params, (err, depart) => {
+    if (err) {
+      res.json('Not a department.');
+      return;
+    }
+    if (depart[0] === undefined) {
+      res.json('Department does not exist.');
+      return
+    } else {
+      params = [depart[0].id];
+      sql = `SELECT * FROM role WHERE department_id = ?`;
+      db.query(sql, params, (err, role) => {
+        if (err) {
+          res.json('No role found for that department.');
+          return;
+        }
+        params = [role[0].id];
+        sql = `SELECT * FROM employee WHERE role_id = ?`;
+        db.query(sql, params, (err, rows) => {
+          if (err) {
+            res.json('No employees found.');
+
+            return;
+          }
+          res.json(
+            {
+              message: 'Success!',
+              data: rows
+            })
+        })
+      })
+    }
+  });
+});
+
+
+//total utilized budget
+router.get('/budget/:id', (req, res) => {
+  let params = [req.params.id];
+  let sql = `SELECT department.id,
+  role.salary,
+  employee.role_id,
+  role.department_id
+  FROM department
+  INNER JOIN role ON department_id = department.id
+  INNER JOIN employee ON role_id = role.id
+  WHERE department.id = ? 
+  `;
+  db.query(sql, params, (err, num) => {
+    if (err) {
+      res.json({ error: err.message });
+      return;
+    } else {
+      let budget = 0;
+      for (let i = 0; i < num.length; i++) {
+        console.log(parseInt(num[i].salary))
+      
+        budget += parseInt(num[i].salary);
+        console.log(budget);
+      }
+    
+      res.json(
+        {
+          message: 'The current budget is:',
+          data: budget
+        }
+      )
+    }
+  });
+});
+
+
 //get single employee
 router.get(`/employee/:id`, (req, res) => {
   const sql = `SELECT * FROM employee WHERE id = ?`
@@ -76,13 +169,29 @@ router.delete('/employee/:id', (req, res) => {
 //update employees role
 router.put(`/employee/:id`, ({body}, res) => {
   const sql = `UPDATE employee
-  SET first_name = ?,
-  last_name =?,
-  role_id = ?,
-  manager_id =?
+  SET role_id = ?
   WHERE id = ?`;
-  const params = [body.first_name, body.last_name, body.role_id, body.manager_id, body.id];
-  db.query(sql, params, (err, results, fields) => {
+  const params = [body.role_id, body.id];
+  db.query(sql, params, (err, results) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json(
+      {
+        message: 'Success!',
+        data: results
+      })
+  });
+});
+
+//update employee manager
+router.put(`/manager/:id`, ({ body }, res) => {
+  const sql = `UPDATE employee
+  SET manager_id =?
+  WHERE id = ?`;
+  const params = [body.manager_id, body.id];
+  db.query(sql, params, (err, results) => {
     if (err) {
       res.status(500).json({ error: err.message });
       return;
