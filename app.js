69597c681037dfbc62c1c2f65dcea8cdcdc69d0b
JPB <jbanas9124@gmail.com>
2021-04-29 14:35:01 -0500

    moving apis into app.js inquirer function

--- app.js
+++ app.js
@@ -1,14 +1,68 @@
 const cTable = require('console.table');
 const inquirer = require('inquirer');
 const express = require('express');
+const app = express();
+const http = require('http');
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 const db = require('./db/connection');
-const apiRoutes = require('./routes/apiRoutes/server');
+const router = require('./routes/apiRoutes/server');
+const { response } = require('express');
 
 //prompt functions
 
 //presented with choices: view all departments, view all roles, view all employees,
 //add department, add role, add employee, update employee role
+const mainMenu = () => {
+  return inquirer.prompt([
+    {
+      type: 'list',
+      message: 'What can I help you do?',
+      name: 'selection',
+      choices: ['View all the departments in my business.', 'View all the roles in the departments.', 'View all my employees.',
+        'I would like to add a new department.', 'Add a new role please.', 'How about we create a new employee?', 'May I change an employee role, please?',
+        'I am all done for now, thank you.']
+    }
+  ])
+    .then(answer => {
+      switch (answer.selection) {
+        case 'View all the departments in my business.':
+          viewDept();
+          console.log('view dept');
+          break;
+        case 'View all the roles in the departments.':
+          console.log('view roles');
+          break;
+        case 'View all my employees.':
+          console.log('view employees');
+          break;
+        case 'I would like to add a new department.':
+          console.log('add dept');
+          break;
+        case 'Add a new role please.':
+          console.log('add role');
+          break;
+        case 'How about we create a new employee?':
+          console.log('create employee');
+          break;
+        case 'May I change an employee role, please?':
+          console.log('change employee role');
+          break;
+        case 'I am all done for now, thank you.':
+          console.log('exit');
+          break;
+      }
+    })
+}
 
+const viewDept = () => {
+  
+}
+
+
+mainMenu();
+module.exports = { mainMenu, viewDept };
 //view all departments, formatted table with department names and ids
 
 //view all roles, table with job title, role id, 
